68fdf710f21ba2070cb71622f7b6b482f92df119
wheeei <wheeei@pop-os.localdomain>
2020-10-21 22:32:38 +0100

    refactoring with PROPS

--- components/src/index.js
+++ components/src/index.js
@@ -1,14 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import faker from "faker";
 import CommentDetail from "./CommentDetail";
+import faker from "faker";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today 4:36 PM"
+        commentText="nice one!"
+        avatarImg={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Jane"
+        timeAgo="Today at 1:00 AM"
+        commentText="Amazing!"
+        avatarImg={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Ben"
+        timeAgo="Yesterday at 3:00PM"
+        commentText="Sweet!"
+        avatarImg={faker.image.avatar()}
+      />
     </div>
   );
 };
