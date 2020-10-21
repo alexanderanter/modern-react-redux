4fd70c2611c283de2113b29430967bc9c3f332a2
wheeei <wheeei@pop-os.localdomain>
2020-10-21 22:51:27 +0100

    nesting components with props.children

--- components/src/index.js
+++ components/src/index.js
@@ -2,28 +2,35 @@ import React from "react";
 import ReactDOM from "react-dom";
 import CommentDetail from "./CommentDetail";
 import faker from "faker";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today 4:36 PM"
-        commentText="nice one!"
-        avatarImg={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Jane"
-        timeAgo="Today at 1:00 AM"
-        commentText="Amazing!"
-        avatarImg={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Ben"
-        timeAgo="Yesterday at 3:00PM"
-        commentText="Sweet!"
-        avatarImg={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today 4:36 PM"
+          commentText="nice one!"
+          avatarImg={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Today at 1:00 AM"
+          commentText="Amazing!"
+          avatarImg={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Ben"
+          timeAgo="Yesterday at 3:00PM"
+          commentText="Sweet!"
+          avatarImg={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
