22573aee253cbf4c91afee58efd824236938e0b8
wheeei <7512280+alexanderanter@users.noreply.github.com>
2020-10-22 13:38:23 +0100

    adding a spinner with props and defaultprops

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
   state = { lat: null, errorMessage: "" };
@@ -21,7 +22,9 @@ class App extends React.Component {
       //we take state from one component and pass it as a prop down to the child
       return <SeasonDisplay lat={this.state.lat} />;
     } else {
-      return <div>Loading!</div>;
+      return (
+        <Spinner spinnerMessage="Please allow to share your location for this app to work!" />
+      );
     }
   }
 }
