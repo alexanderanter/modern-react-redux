205e2ca63a0e4dbf8d5d133e0416da325694ca08
wheeei <7512280+alexanderanter@users.noreply.github.com>
2020-10-22 12:16:59 +0100

    passing state as a prop to a child

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -17,7 +17,9 @@ class App extends React.Component {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     } else if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+      //taking a property from the state on the app component and passing as a prop into the SeasonDisplay component
+      //we take state from one component and pass it as a prop down to the child
+      return <SeasonDisplay lat={this.state.lat} />;
     } else {
       return <div>Loading!</div>;
     }
