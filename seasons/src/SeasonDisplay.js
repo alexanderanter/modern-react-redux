5f956c59aeef216880bcb3141a0f88bce58aaa19
wheeei <7512280+alexanderanter@users.noreply.github.com>
2020-10-22 12:27:36 +0100

    ternery expression in JSX

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -10,8 +10,12 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
-  console.log(season);
-  return <div>Season Display</div>;
+
+  return (
+    <div>
+      {season === "winter" ? "Burr, it's chilly" : "Let's hit the beach"}
+    </div>
+  );
 };
 
 export default SeasonDisplay;
