d77b8947032339c95f86ff4045ca9db99d99a905
wheeei <7512280+alexanderanter@users.noreply.github.com>
2020-10-23 10:37:07 +0100

    basic onchange event handler

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,17 +1,25 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
+
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
-            <label for="searchfield">Search term</label>
+            <label htmlFor="searchfield">Search term</label>
             <input
               name="searchfield"
               type="text"
               placeholder="fill in your searchterm"
-            ></input>
+              onChange={
+                this.onInputChange
+                // DO NOT put () on functions when passing them to an event handler like onChange, that would call the function everytime its rendered instead of everytime there is a change.
+              }
+            />
           </div>
         </form>
       </div>
