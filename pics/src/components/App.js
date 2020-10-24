400a7e14ff82761544b6e434917038c879b32d16
wheeei <7512280+alexanderanter@users.noreply.github.com>
2020-10-24 11:23:01 +0100

    displaying amount of images with a this bug

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -3,6 +3,8 @@ import SearchBar from "./SearchBar";
 import axios from "axios";
 
 class App extends React.Component {
+  state = { images: [] };
+
   async onSearchSubmit(term) {
     const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: term },
@@ -10,13 +12,15 @@ class App extends React.Component {
         Authorization: "Client-ID MSiz2TTBQvP9bit6sPIO2GSHkgZ7X_vAk1f-nHc2Ru8",
       },
     });
-    console.log(response.data.results);
+
+    this.setState({ images: response.data.results });
   }
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
     );
   }
