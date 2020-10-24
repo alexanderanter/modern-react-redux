78544769f66f536e3742e71465e3581919e10e1a
wheeei <7512280+alexanderanter@users.noreply.github.com>
2020-10-24 14:35:05 +0100

    adding some comments

--- pics/src/components/ImageCard.js
+++ pics/src/components/ImageCard.js
@@ -3,24 +3,27 @@ class ImageCard extends React.Component {
   constructor(props) {
     super(props);
     this.state = { spans: 0 };
+    //step 1declare and create the REF
     this.imageRef = React.createRef();
   }
 
   componentDidMount() {
-    //add a load callback to call setSpans function when its loaded
+    //add a load callback to call setSpans function when the element we reach via REF is loaded
     this.imageRef.current.addEventListener("load", this.setSpans);
   }
   //make sure its an arrow function to avoid issues with this
   setSpans = () => {
+    //check the value of our referenced element
     const height = this.imageRef.current.clientHeight;
     const spans = Math.ceil(height / 10);
-
+    //update the state and rerender
     this.setState({ spans });
   };
   render() {
     const { description, urls } = this.props.image;
     return (
       <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+        {/* create ref attribute that points to the declared ref in the constructor */}
         <img ref={this.imageRef} alt={description} src={urls.regular} />
       </div>
     );
