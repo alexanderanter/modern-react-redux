47cdb0c016faa0fbbf7417dfbc6a783e33e42fac
wheeei <7512280+alexanderanter@users.noreply.github.com>
2020-10-27 13:11:00 +0000

    youtube api request implemented

--- videos/src/apis/youtube.js
+++ videos/src/apis/youtube.js
@@ -5,7 +5,7 @@ const KEY = "AIzaSyAdWkgk8000oSw4kRk0P5N9IFR3QJtNdrw";
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
   params: {
-    snippet: "snippet",
+    part: "snippet",
     type: "video",
     maxResults: 5,
     key: KEY,
