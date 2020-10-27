8f68ca8731c541505b090d45b679bb686cceed72
wheeei <7512280+alexanderanter@users.noreply.github.com>
2020-10-27 17:01:14 +0000

    adding in thumbnails child to parent

--- videos/src/components/VideoItem.js
+++ videos/src/components/VideoItem.js
@@ -1,13 +1,25 @@
 import React from "react";
+import "./VideoItem.css";
 
-const VideoItem = (props) => {
+const VideoItem = ({ video }) => {
+  console.log(video);
   return (
     <div>
-      <iframe
-        width="420"
-        height="315"
-        src={`https://www.youtube.com/embed/${props.video.id.videoId}`}
-      ></iframe>
+      <div className="video-item item">
+        <img
+          className="video-thumb ui image"
+          width="300px"
+          src={video.snippet.thumbnails.medium.url}
+        />
+        <div className="content">
+          <div className="header"> {video.snippet.title}</div>
+        </div>
+        {/* <iframe
+          width="100%"
+          height="315"
+          src={`https://www.youtube.com/embed/${video.id.videoId}`}
+        ></iframe> */}
+      </div>
     </div>
   );
 };
--- videos/src/components/VideoList.js
+++ videos/src/components/VideoList.js
@@ -1,11 +1,14 @@
 import React from "react";
 import VideoItem from "./VideoItem";
+import "./VideoList.css";
 
 const VideoList = ({ videos = [] }) => {
   const renderedList = videos.map((video) => {
     return <VideoItem video={video} key={video.id.videoId} />;
   });
-  return <div>{renderedList}</div>;
+  return (
+    <div className="video-thumbs ui relaxed divided list">{renderedList}</div>
+  );
 };
 
 export default VideoList;
