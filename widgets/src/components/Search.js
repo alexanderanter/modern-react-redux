24162bcabcdc8598d927b9dacb7a6da83a93fa7b
wheeei <7512280+alexanderanter@users.noreply.github.com>
2020-10-30 17:15:55 +0000

    make an async api call to wikipedia inside useEffect hook

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,12 +1,29 @@
 // http://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=SEARCHTERM
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Search = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const handleOnChange = (value) => {
     setSearchTerm(value);
   };
+
+  useEffect(() => {
+    const search = async () => {
+      await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: searchTerm,
+        },
+      });
+    };
+    search();
+  }, [searchTerm]);
+
   return (
     <div className="ui form">
       <div className="field">
