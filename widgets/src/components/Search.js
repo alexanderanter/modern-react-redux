c56de199d5d044a8124366674c9011ffe03d82dd
wheeei <7512280+alexanderanter@users.noreply.github.com>
2020-10-30 17:35:18 +0000

    display results with a possibible vurnability using dangerouslySetInnerHTML

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -10,6 +10,7 @@ const Search = () => {
   const handleOnChange = (value) => {
     setSearchTerm(value);
   };
+  console.log(results);
   useEffect(() => {
     const search = async () => {
       const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
@@ -29,17 +30,29 @@ const Search = () => {
       search();
     }
   }, [searchTerm]);
-
-  return (
-    <div className="ui form">
-      <div className="field">
-        <label> Enter Search input</label>
-        <input
-          className="input"
-          value={searchTerm}
-          onChange={(e) => handleOnChange(e.target.value)}
-        />
+  const renderedResults = results.map((result) => {
+    return (
+      <div className="item" key={result.pageid}>
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
       </div>
+    );
+  });
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label> Enter Search input</label>
+          <input
+            className="input"
+            value={searchTerm}
+            onChange={(e) => handleOnChange(e.target.value)}
+          />
+        </div>
+      </div>
+      <div className="ui celled list ">{renderedResults}</div>
     </div>
   );
 };
