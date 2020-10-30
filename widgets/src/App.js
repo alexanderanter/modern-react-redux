63cbac2b6a28ef1623f5a7802ca850afb9e78279
wheeei <7512280+alexanderanter@users.noreply.github.com>
2020-10-30 15:06:16 +0000

    passing down an array as a prop

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,5 +1,25 @@
 import React from "react";
+import Accordion from "./components/Accordion";
+
+const items = [
+  {
+    title: "What is React",
+    content: "React is a front end js framework",
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favorite JS lib",
+  },
+  {
+    title: "how do you use react?",
+    content: "You use it by creating components",
+  },
+];
 
 export default () => {
-  return <h1>Widgets App</h1>;
+  return (
+    <div>
+      <Accordion items={items} />
+    </div>
+  );
 };
