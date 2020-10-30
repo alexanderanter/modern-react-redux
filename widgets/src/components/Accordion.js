c7e3d00cbe65354ad620207f592a2ea47357a47f
wheeei <7512280+alexanderanter@users.noreply.github.com>
2020-10-30 15:25:09 +0000

    calling a helper function in a function based component

--- widgets/src/components/Accordion.js
+++ widgets/src/components/Accordion.js
@@ -1,10 +1,15 @@
 import React from "react";
 
 const Accordion = ({ items }) => {
-  const renderedItems = items.map((item) => {
+  const onTitleClick = (index) => {
+    console.log("Title clicked", `item-${index}`);
+  };
+  const renderedItems = items.map((item, index) => {
     return (
-      <React.Fragment key={item.title}>
-        <div className="title active">
+      <React.Fragment key={`item-${index}`}>
+        {/* make sure you call onTitleClick with an arrowfunction
+          otherwise the function would be called when everything gets rendered and not when clicking! */}
+        <div onClick={() => onTitleClick(index)} className="title active">
           <i className="dropdown icon"></i>
           {item.title}
         </div>
