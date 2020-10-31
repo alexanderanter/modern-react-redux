f0ac9223172929cabd69a2e18820ff74f42596c8
alexanderanter <7512280+alexanderanter@users.noreply.github.com>
2020-10-31 15:23:38 +0000

    refactored code using a second useeffect hook

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -27,7 +27,21 @@ const Search = () => {
 	const handleOnChange = (value) => {
 		setSearchTerm(value);
 	};
+	useEffect(() => {
+		const timeoutId = setTimeout(() => {
+			//set the debouncedSearchTerm to the new searchTerm
+			setDebouncedSearchTerm(searchTerm);
+		}, 500);
+		//whats in the return statement of the useEffect does not get triggered upon initial render
+		// it will be saved in memory and invoked immedeitely upon next render, perfect for cleanup!
+		return () => {
+			//run this code NEXT time it gets rendered!
+			clearTimeout(timeoutId);
+		};
+	}, [searchTerm]);
 
+	// only get run initial and when debouncedSearchTerm CHANGES so it avoids making multiple requests
+	// for the same phrase
 	useEffect(() => {
 		const search = async () => {
 			const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
@@ -41,26 +55,9 @@ const Search = () => {
 			});
 			setResults(data.query.search);
 		};
-		//check if there is a default term and no results, in that case
-		// run the search instantly on first render
-		if (searchTerm && !results.length) {
-			search();
-			//only search after a searchterm have been provided and THAT IS NOT like the previous one!
-		} else if (searchTerm && searchTerm !== debouncedSearchTerm) {
-			//add a timeout and place the id in a variabel so we can clear it in the return cleanup function
-			const timeoutId = setTimeout(() => {
-				search();
-			}, 500);
-			//whats in the return statement of the useEffect does not get triggered upon initial render
-			// it will be saved in memory and invoked immedeitely upon next render, perfect for cleanup!
-			return () => {
-				//set the previous searchterm to debouncedSearchTerm
-				setDebouncedSearchTerm(searchTerm);
-				//run this code NEXT time it gets rendered!
-				clearTimeout(timeoutId);
-			};
-		}
-	}, [searchTerm, debouncedSearchTerm, results.length]);
+		search(debouncedSearchTerm);
+	}, [debouncedSearchTerm]);
+
 	const renderedResults = results.map((result) => {
 		const sanitizedResult = sanitize(result.snippet);
 
