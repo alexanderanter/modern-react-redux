3738ccb94680cf8f58aedf60c935fbcf42251618
alexanderanter <7512280+alexanderanter@users.noreply.github.com>
2020-10-31 23:29:03 +0000

    refactored so text gets translateda as you type

--- widgets/src/components/Convert.js
+++ widgets/src/components/Convert.js
@@ -1,37 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Convert = ({ language, text }) => {
 	const [translatedText, setTranslatedText] = useState('');
-	const translate = async () => {
-		const { data } = await axios.get(
-			'https://translation.googleapis.com/language/translate/v2',
-			{
-				params: {
-					q: text,
-					target: language.value,
-					key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM',
-				},
-			}
-		);
-		console.log(data.data.translations[0].translatedText);
 
-		setTranslatedText(data.data.translations[0].translatedText);
-	};
-	const handleTranslate = () => {
+	useEffect(() => {
+		const translate = async () => {
+			const { data } = await axios.get(
+				'https://translation.googleapis.com/language/translate/v2',
+				{
+					params: {
+						q: text,
+						target: language.value,
+						key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM',
+					},
+				}
+			);
+			console.log(data.data.translations[0].translatedText);
+
+			setTranslatedText(data.data.translations[0].translatedText);
+		};
 		translate();
-	};
+	}, [text]);
+
 	return (
 		<div>
-			<p>{language.label}</p>
-			<p>{text}</p>
-			<button
-				onClick={() => {
-					handleTranslate();
-				}}
-			>
-				TRANSLATE
-			</button>
 			<p>{translatedText}</p>
 		</div>
 	);
