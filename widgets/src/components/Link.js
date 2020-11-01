dd0f1b706db4e8f1a9e7bba2bf2b16bf96e05d85
alexanderanter <7512280+alexanderanter@users.noreply.github.com>
2020-11-01 10:41:34 +0000

    handle command clicks for default behaviour

--- widgets/src/components/Link.js
+++ widgets/src/components/Link.js
@@ -2,6 +2,11 @@ import React from 'react';
 
 const Link = ({ className, href, children }) => {
 	const onClick = (event) => {
+		if (event.metaKey || event.ctrlKey) {
+			//if a user holds control or command simply exit in order
+			// for standard browser behaviour (open the link a new tab)
+			return;
+		}
 		event.preventDefault();
 		window.history.pushState({}, '', href);
 
