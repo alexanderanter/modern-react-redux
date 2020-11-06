78b538bc88b6975cfa66005c3519557cfa53fdfb
alexanderanter <7512280+alexanderanter@users.noreply.github.com>
2020-11-06 23:55:46 +0000

    calling redux actions creators from components

--- songs/src/components/SongList.js
+++ songs/src/components/SongList.js
@@ -1,28 +1,37 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectSong } from '../actions';
 
 class SongList extends Component {
 	renderedSongs() {
 		return this.props.songs.map((song) => {
+			console.log(this.props.selectedSong);
 			return (
 				<div className="item" key={song.title}>
 					{song.title}
 					<div className="right floated content">
-						<button className="ui button primary">Select</button>
+						<button
+							className="ui button primary"
+							onClick={() => this.props.selectSong(song)}
+						>
+							Select
+						</button>
 					</div>
+					<b>{song.title === this.props.selectedSong.title && ' selected'}</b>
 				</div>
 			);
 		});
 	}
 	render() {
-		console.log(this.props);
 		return <div className="ui divided list">{this.renderedSongs()}</div>;
 	}
 }
 
 const mapStateToProps = (state) => {
-	return { songs: state.songs };
+	return { songs: state.songs, selectedSong: state.selectedSong };
 };
 
 //invoke the function inside the connect function
-export default connect(mapStateToProps)(SongList);
+export default connect(mapStateToProps, {
+	selectSong: selectSong,
+})(SongList);
