2fb34adbde4ea7412802065ee538bd3e8f072e31
alexanderanter <7512280+alexanderanter@users.noreply.github.com>
2020-11-07 05:18:29 +0000

    fixing async action with redux-thunk

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,15 +1,9 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
-export const fetchPosts = async () => {
+export const fetchPosts = () => async (dispatch) => {
 	const response = await jsonPlaceholder.get('/posts');
-	//bad code, an action that does not return a plain JS object
-	//after transpiling this to es2015 code this will not return an object
-	//even though it looks like it! Async - Await creates lots of stuff in the background
-
-	//By the time our gets to our reducer we wont have fetched our data
-	//Error: Actions must be plain objects. Use custom middleware for async actions.
-	return {
+	dispatch({
 		type: 'FETCH_POSTS',
 		payload: response,
-	};
+	});
 };
