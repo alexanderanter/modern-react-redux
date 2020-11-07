ce92f5ad1db28f76aac01a160679464fffded86c
alexanderanter <7512280+alexanderanter@users.noreply.github.com>
2020-11-07 04:59:39 +0000

    implementing a bug into action creator

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,5 +1,15 @@
-export const fetchPosts = () => {
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPosts = async () => {
+	const response = await jsonPlaceholder.get('/posts');
+	//bad code, an action that does not return a plain JS object
+	//after transpiling this to es2015 code this will not return an object
+	//even though it looks like it! Async - Await creates lots of stuff in the background
+
+	//By the time our gets to our reducer we wont have fetched our data
+	//Error: Actions must be plain objects. Use custom middleware for async actions.
 	return {
 		type: 'FETCH_POSTS',
+		payload: response,
 	};
 };
