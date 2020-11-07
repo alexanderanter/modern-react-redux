756c8cbffdae5e0a4d07116f5274eedc244ee83a
alexanderanter <7512280+alexanderanter@users.noreply.github.com>
2020-11-07 16:42:18 +0000

    loading in the username for each blogposts

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -7,3 +7,10 @@ export const fetchPosts = () => async (dispatch) => {
 		payload: response.data,
 	});
 };
+export const fetchUser = (userId) => async (dispatch) => {
+	const response = await jsonPlaceholder.get(`/users/${userId}`);
+	dispatch({
+		type: 'FETCH_USER',
+		payload: response.data,
+	});
+};
