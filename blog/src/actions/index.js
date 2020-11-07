53c26f215ea1e73fd04b5fce9526293eed040f14
alexanderanter <7512280+alexanderanter@users.noreply.github.com>
2020-11-07 17:29:24 +0000

    fixing multiple api requests with memoize (lodash)

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,16 +1,17 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+import _ from 'lodash';
 
 export const fetchPosts = () => async (dispatch) => {
 	const response = await jsonPlaceholder.get('/posts');
-	dispatch({
-		type: 'FETCH_POSTS',
-		payload: response.data,
-	});
+
+	dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
-export const fetchUser = (userId) => async (dispatch) => {
-	const response = await jsonPlaceholder.get(`/users/${userId}`);
-	dispatch({
-		type: 'FETCH_USER',
-		payload: response.data,
-	});
-};
+
+export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
+
+//only fetch each user ONE time from the API
+const _fetchUser = _.memoize(async (id, dispatch) => {
+	const response = await jsonPlaceholder.get(`/users/${id}`);
+
+	dispatch({ type: 'FETCH_USER', payload: response.data });
+});
